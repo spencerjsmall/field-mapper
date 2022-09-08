84699f51ecdc7d7a3fc9b61a9769476a7d9596f7
spencerjsmall <spencer_small@brown.edu>
2022-09-08 08:51:34 -0700

    Add basemap selector, fix viewport

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 import type { LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
 
 import Map, {
   Source,
@@ -15,6 +15,7 @@ import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-direct
 
 import Layout from "~/components/layout";
 import { getFirstPt } from "~/utils/test.server";
+import clsx from "clsx";
 
 export function links() {
   return [
@@ -40,8 +41,9 @@ export const loader = async ({ request }: LoaderArgs) => {
 };
 
 export default function MapBox() {
-  const data = useLoaderData();  
-  const [showPopup, setShowPopup] = useState(false);  
+  const data = useLoaderData();
+  const [showPopup, setShowPopup] = useState(false);
+  const [basemap, setBasemap] = useState("streets-v11");
   const [dCoords, setDCoords] = useState({ lng: 0, lat: 0 });
   const [cCoords, setCCoords] = useState({ lng: 0, lat: 0 });
 
@@ -99,53 +101,94 @@ export default function MapBox() {
 
   return (
     <Layout>
-      <Map
-        initialViewState={{
-          longitude: -122.44,
-          latitude: 37.75,
-          zoom: 12,
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={
-          "pk.eyJ1Ijoic3BlbmNlcmpzbWFsbCIsImEiOiJjbDdmNGY5d2YwNnJuM3hsZ2IyN2thc2QyIn0.hLfNqU8faCraSSKrXbtnHQ"
-        }
-        interactiveLayerIds={["bike_data"]}
-        onClick={onFeatureClick}
-      >
-        {/* <Source
-          id="tiles"
-          type="raster"
-          tiles={["https://til.3dg.is/api/tiles/p2021_rgb8cm/{z}/{x}/{y}.png"]}
-          tileSize={256}
+      <div className="h-full relative">
+        <Map
+          initialViewState={{
+            longitude: -122.44,
+            latitude: 37.75,
+            zoom: 12,
+          }}
+          mapStyle={
+            basemap == "custom"
+              ? "mapbox://styles/mapbox/streets-v11"
+              : `mapbox://styles/mapbox/${basemap}`
+          }
+          mapboxAccessToken={
+            "pk.eyJ1Ijoic3BlbmNlcmpzbWFsbCIsImEiOiJjbDdmNGY5d2YwNnJuM3hsZ2IyN2thc2QyIn0.hLfNqU8faCraSSKrXbtnHQ"
+          }
+          interactiveLayerIds={["bike_data"]}
+          onClick={onFeatureClick}
         >
-          <Layer type="raster" />
-        </Source> */}
-        <Source id="my-data" type="geojson" data={data}>
-          <Layer id="bike_data" {...layerStyle} />
-        </Source>
-        {showPopup && (
-          <Popup
-            longitude={dCoords.lng}
-            latitude={dCoords.lat}
-            anchor="bottom"
-            onClose={() => setShowPopup(false)}        
-          >
-            <div className="flex flex-col items-center space-y-2">
-              <button
-                onClick={getDirections}
-                className="btn btn-xs btn-outline btn-primary"
-              >
-                Get Directions
-              </button>
-              <button className="btn btn-xs btn-outline btn-secondary">
-                Complete Survey
-              </button>
+          {basemap == "custom" && (
+            <Source
+              id="tiles"
+              type="raster"
+              tiles={[
+                "https://til.3dg.is/api/tiles/p2021_rgb8cm/{z}/{x}/{y}.png",
+              ]}
+              tileSize={256}
+            >
+              <Layer type="raster" />
+            </Source>
+          )}
+          <Source id="my-data" type="geojson" data={data}>
+            <Layer id="bike_data" {...layerStyle} />
+          </Source>
+          {showPopup && (
+            <Popup
+              longitude={dCoords.lng}
+              latitude={dCoords.lat}
+              anchor="bottom"
+              onClose={() => setShowPopup(false)}
+            >
+              <div className="flex flex-col items-center space-y-2">
+                <button
+                  onClick={getDirections}
+                  className="btn btn-xs btn-outline btn-primary"
+                >
+                  Get Directions
+                </button>
+                <Link to="/survey">
+                  <button className="btn btn-xs btn-outline btn-secondary">
+                    Complete Survey
+                  </button>
+                </Link>
+              </div>
+            </Popup>
+          )}
+          <GeolocateControl
+            onGeolocate={setCurrentLocation}
+            ref={geolocateRef}
+          />
+          <DirectionsControl />
+        </Map>
+        <ul className="menu menu-horizontal bg-base-100 w-auto absolute top-1 left-1 text-xs p-1 rounded-box">
+          <li>
+            <div
+              onClick={() => setBasemap("streets-v11")}
+              className={clsx("p2", { active: basemap == "streets-v11" })}
+            >
+              Traffic
             </div>
-          </Popup>
-        )}
-        <GeolocateControl onGeolocate={setCurrentLocation} ref={geolocateRef} />
-        <DirectionsControl />
-      </Map>
+          </li>
+          <li>
+            <div
+              onClick={() => setBasemap("dark-v10")}
+              className={clsx("p2", { active: basemap == "satellite-v9" })}
+            >
+              Dark
+            </div>
+          </li>
+          <li>
+            <div
+              onClick={() => setBasemap("custom")}
+              className={clsx("p2", { active: basemap == "custom" })}
+            >
+              Custom
+            </div>
+          </li>
+        </ul>
+      </div>
     </Layout>
   );
 }
