9bc536c20aee01067f15258132f1ff00f1ea7262
spencerjsmall <spencer_small@brown.edu>
2022-09-24 13:17:37 -0700

    Minor tweaks

--- app/routes/admin/tasks/$.tsx
+++ app/routes/admin/tasks/$.tsx
@@ -2,14 +2,12 @@ import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params }: LoaderArgs) => {
   const pathname = params["*"];
-  console.log('pathname', pathname)
   const ids = pathname.split("/").map((i) => parseInt(i));
   return ids
 };
 
 export default function SideBar() {
   const ids = useLoaderData();  
-  console.log('ids', ids)  
   return (
     <div>
       <ul className="justify-center items-center flex flex-col space-y-2">
